4a5f3d909421be7182d30c031a233769da1b08db
Nick <38103950+Senpai-Saiyan@users.noreply.github.com>
2018-05-31 13:58:57 -0400

    Update RP.js

--- RP.js
+++ RP.js
@@ -71,14 +71,3 @@ process.on('uncaughtException', exitHandler.bind(null, {
 
 client.login(data.dtoken);
 console.log('Logging in!');
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
